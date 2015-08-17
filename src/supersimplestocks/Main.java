e6cfb3b666dab895bb650952c98af141d9e4e010
alarv <alarvfm@gmail.com>
2015-08-17 21:03:20 +0300

    Finished all calculations, haven't tested yet.

--- src/supersimplestocks/Main.java
+++ src/supersimplestocks/Main.java
@@ -5,10 +5,12 @@
  */
 package supersimplestocks;
 
-import java.io.Console;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import org.joda.time.DateTime;
 
 /**
  *
@@ -35,10 +37,14 @@ public class Main {
         stockData.put(StockSymbol.GIN, GIN);
         stockData.put(StockSymbol.JOE, JOE);
 
+        List<Trade> tradeList = new ArrayList<>();
+        List<Float> marketPriceList = new ArrayList<>();
+        boolean addMore = false;
+        
         do {
             //read values needed
             Scanner in = new Scanner(System.in);
-            System.out.print("Please enter stock symbol: ");
+            System.out.print("Please enter stock symbol(TEA/POP/ALE/GIN/JOE): ");
             String stockSymbolStr = in.nextLine();
 
             if (!StockSymbol.isMember(stockSymbolStr)) {
@@ -61,10 +67,45 @@ public class Main {
                 System.err.println("Market price cannot be zero. Exiting...");
                 System.exit(0);
             }
-
+            
+            marketPriceList.add(marketPrice);
+            
             StockSymbol stockSymbol = StockSymbol.valueOf(stockSymbolStr);
             Stock givenStock = stockData.get(stockSymbol);
-
+            
+            //Get trade
+            System.out.println("Recording Trade...");
+            
+            //quantity of shares
+            System.out.print("Please enter quantity of shares: ");
+            String quantitySharesStr = in.nextLine();
+            int sharesQuantity = 0;
+            try {
+                sharesQuantity = Integer.parseInt(quantitySharesStr);
+            } catch (NumberFormatException e) {
+                System.err.println("The number given is invalid! Exiting...");
+                System.exit(0);
+            }
+            
+            //sell indicator
+            System.out.print("Please enter buy/sell indicator (buy/sell): ");
+            String buySellIndicatorStr = in.nextLine();
+            SellIndicator sellIndicator = SellIndicator.valueOf(buySellIndicatorStr.toUpperCase());
+            
+            //trade price
+            System.out.print("Please enter trade price: ");
+            String tradePriceStr = in.nextLine();
+            float tradePrice = 0;
+            try {
+                tradePrice = Float.parseFloat(tradePriceStr);
+            } catch (NumberFormatException e) {
+                System.err.println("The number given is invalid! Exiting...");
+                System.exit(0);
+            }
+            
+            Trade newTrade = new Trade(new DateTime(), sharesQuantity, sellIndicator, tradePrice);
+            tradeList.add(newTrade);
+            
             //Calculate values
             //Dividend Yield
             float dividendYield = 0;
@@ -83,10 +124,43 @@ public class Main {
             float peRatio = (float) (marketPrice / (givenStock.getLastDividend() != 0 ? givenStock.getLastDividend() : 1.0));
             System.out.println("The P/E ratio is: " + peRatio);
             
+            //Volume Weighted Stock Price
+            float vwspNumerator = 0;
+            float vwspDenominator = 0;
+            
+            for(Trade trade : tradeList){
+                DateTime before15minutes = DateTime.now().minusMinutes(15);
+                if(trade.getTimestamp().isAfter(before15minutes)){
+                    //if trade belongs in the past 15'
+                    vwspNumerator += trade.getSharesQuantity() * trade.getTradePrice();
+                    vwspDenominator += trade.getSharesQuantity();
+                }
+            }
+            
+            //avoid division by zero
+            float volumeWeightedStockPrice = (float) (vwspNumerator / (vwspDenominator != 0.0 ? vwspDenominator : 1.0));
+            
+            System.out.println("Volume Weighted Stock Price is: "+ volumeWeightedStockPrice);
             
             
-        } while (false);
+            System.out.print("Do you want to add more stocks? (y/n): ");
+            String yesNoStr = in.nextLine();
+            if(yesNoStr.equals("y")){
+               addMore = true; 
+            } else {
+                addMore = false; 
+            }
+             
+        } while (addMore);
 
+        //finally print the GBCE All Share Index
+        double marketPriceProduct = 1.0;
+        for(double marketPrice : marketPriceList){
+            marketPriceProduct *= marketPrice;
+        }
+        
+        System.out.println("GCBE All Share Index: " + Math.pow(marketPriceProduct, 1 / marketPriceList.size()));
+        
     }
 
 }
